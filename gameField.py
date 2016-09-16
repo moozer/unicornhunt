729d753d9f4260078954ed70d092164222a60adf
moz <moz@local>
2016-09-16 21:13:05 +0200

    added gamefield class

---
+++ gameField.py
@@ -0,0 +1,87 @@
+import pygame
+from pygame.locals import *
+from collections import namedtuple
+
+point = namedtuple( 'point', ['x', 'y'] )
+tileinfo = namedtuple( 'tileInfo', ['filename', 'symbol'] )
+
+tileList = { 'grass': tileinfo( "images/grass.png", "s" ) }
+
+def debugprint( str ):
+    print str
+
+class tile():
+    def __init__( self, filename ):
+        self._image = pygame.image.load( filename )
+    
+    @property
+    def image( self ):
+        return self._image
+
+class gameField():
+    def __init__( self, tilesGeo, tileList = tileList, debugprint = debugprint ):
+        self._print = debugprint
+
+        self._tileSize = point( 32, 32 )
+        self._tilesGeo = tilesGeo
+        self._tileList = tileList
+        self._loadTiles()
+
+        self._size = (self._tileSize.x * self._tilesGeo.x,  self._tileSize.y * self._tilesGeo.y ) 
+        self._print("Game field size is %s"%(self._size, ) )
+        self._initGraphics()
+
+    def _loadTiles( self ):
+        self._print("Loading tiles")
+        self._tiles = []
+        for t in self._tileList.keys():
+            self._print("- loading %s"%(t,) )
+            self._tiles.append( tile( self._tileList[t].filename ) )
+    
+    def _addTiles( self ):
+        self._print("Populating game field")
+        for x in range( 0, self._tilesGeo.x ):
+            for y in range( 0, self._tilesGeo.y ):
+                self._screen.blit( self._tiles[0].image, (x*self._tileSize.x, y*self._tileSize.y) )
+    
+    def _initGraphics( self ):
+        self._screen = pygame.Surface(self._size)
+        self._screen = self._screen.convert()
+        self._addTiles()
+        
+    @property
+    def screen( self):
+        return self._screen
+        
+    
+if __name__ == "__main__":
+    pygame.init()
+    FPS = 30 # frames per second setting
+    fpsClock = pygame.time.Clock()
+
+    screenSize = point( 32*30, 32*20)
+    gfOffset = point( 0,0 )
+
+    # Initialise screen
+    pygame.init()
+    screen = pygame.display.set_mode(screenSize)
+    pygame.display.set_caption('game field test program')
+
+    # --- game field part -------
+    gf = gameField( point( 30, 20 ) )
+    # --- game field part -------
+
+    # Blit everything to the screen
+    screen.blit( gf.screen, gfOffset )
+    pygame.display.flip()
+
+    # Event loop
+    quitLoop = False
+    while not quitLoop:
+        for event in pygame.event.get():
+            if event.type == QUIT:
+                quitLoop = True
+
+        screen.blit(gf.screen, gfOffset)
+        pygame.display.flip()
+
